757c83e50b61d0b04cd490dbffb24f8b752028e4
hsolemio-lee <hslyjy@gmail.com>
2020-08-25 08:57:43 +0900

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,55 @@
-import firebase from 'firebase/app';
-import 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase';
 
+
+class Fire {
+  constructor() {
+    this.init();
+  }
+
+  init = () => {
+    if(!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }
+
+  send = messages => {
+    messages.forEach(item => {
+      const message = {
+        text: item.text,
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        user: item.user
+      }
+  
+      this.db.push(message);
+    })
+  }
+
+  parse = message => {
+    const {user, text, timestamp} = message.val();
+    const {key: _id} = message;
+    const createAt = new Date(timestamp);
+
+    return {
+      _id,
+      createAt,
+      text,
+      user
+    }
+  }
+
+  get = callback => {
+    this.db.on('child_added', snapshot => callback(this.parse(snapshot)));
+  }
+
+  off() {
+    this.db.off();
+  }
+  
+  get db() {
+    return firebase.database().ref('messages');
+  }  
+
+}
 const firebaseConfig = {
     apiKey: "AIzaSyAF2vIPr6RYFUuWcfbBKXo1C0GBl3jsOlQ",
     authDomain: "weatheryouandi.firebaseapp.com",
@@ -11,10 +59,8 @@ const firebaseConfig = {
     messagingSenderId: "400247724525",
     appId: "1:400247724525:web:f7517e53193c63e38e2fe4",
     measurementId: "G-RKNMX4JK3P"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
+export default new Fire();
 
-  const firestore = new firebase.firestore();
-
-  export {firestore};
+export const firestore = new firebase.firestore();
